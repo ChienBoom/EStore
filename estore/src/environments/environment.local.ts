e63f94dbc32fbf151d2bea15b7bf720ff19ff900
Chien BV <chienbv@aratech.vn>
2024-08-25 15:12:48 +0700

    config environment

--- estore/src/environments/environment.local.ts
+++ estore/src/environments/environment.local.ts
@@ -1,5 +1,10 @@
+// export const environment = {
+//   domain: 'http://localhost:6001',
+//   production: false,
+//   port: 6001
+// }
 export const environment = {
-  domain: 'http://localhost:6001',
+  domain: 'http://192.168.1.94:3006',
   production: false,
-  port: 6001
-}
+  port: 3006,
+};
--- estore/src/environments/environment.prod.ts
+++ estore/src/environments/environment.prod.ts
@@ -1,4 +1,11 @@
+// export const environment = {
+//   production: true,
+//   port: 6001
+// }
+
+
 export const environment = {
-  production: true,
-  port: 6001
-}
+  domain: 'http://192.168.1.94:3006',
+  production: false,
+  port: 3006,
+};
